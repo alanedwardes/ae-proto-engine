29828f1fd621282287f5591461161c288d83ed7e
Alan Edwardes <alan@alanedwardes.com>
2014-02-14 00:55:02 +0000

    Fixed leftover SFML variables in the WorldRenderer.

--- src/game/client/WorldRenderer.h
+++ src/game/client/WorldRenderer.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#include "sfml\Graphics.hpp"
 #include "Point.h"
 #include "View.h"
 #include "Locator.h"
 
 class IRendered;
 class ISimulated;
-struct RenderedPolygon;
 class BaseGameObject;
 
 class WorldRenderer : public MainView
@@ -20,9 +18,6 @@ private:
 	virtual void DrawRenderable(BaseGameObject *pEntity, IRendered* pRenderable);
 	virtual void DrawSimulated(BaseGameObject *pEntity, ISimulated* pSimulated);
 	virtual void DrawDebugText(BaseGameObject *pEntity);
-	sf::RenderWindow m_oRenderWindow;
-	sf::RenderTarget* m_pWindowTarget;
-	sf::Image m_oWindowIcon;
 	int m_iDebugFont;
 	float m_flZoomLevel;
 	Camera m_oCamera;
